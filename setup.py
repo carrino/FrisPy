d7e490f0fc9f5c2b271c9ab3b97261a83b8dcd30
John Carrino <john.carrino@gmail.com>
2021-11-10 10:05:08 -0800

    remove unused files

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="frispy",
-    author="Tom McClintock",
-    author_email="thmsmcclintock@gmail.com",
+    name="frispy_service",
+    author="John Carrino",
+    author_email="john.carrino@gmail.com",
     version=frispy.__version__,
     description=frispy.__docs__,
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/tmcclintock/FrisPy",
+    url="https://github.com/carrino/FrisPy",
     packages=["frispy"],
     classifiers=[
         "Programming Language :: Python :: 3",
