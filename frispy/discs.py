e7ead3a38323be37df49348fbd8958265eb598da
John Carrino <john.carrino@gmail.com>
2022-05-03 19:57:51 -0700

    add flippy destroyer

--- frispy/discs.py
+++ frispy/discs.py
@@ -17,6 +17,8 @@ class Discs:
             return Discs.flick
         elif name == "stable_wraith":
             return Discs.stable_wraith
+        elif name == "flippy_destroyer":
+            return Discs.flippy_destroyer
         elif name == "destroyer":
             return Discs.destroyer
         elif name == "stable_destroyer":
@@ -105,6 +107,32 @@ class Discs:
         "diameter": 0.211,
         "rim_depth": 0.012,
         "rim_width": 0.0215,
+        "height": 0.014,
+    })
+
+    flippy_destroyer: Model = Model(**{
+        "PL0": 0.16,  # lift factor at 0 AoA (depends on glide) 2 deg Aoa at 58 mph lift was about 8m/s^2   0.8g
+        "PLa": 2.29,  # lift factor linear with AoA (0.04 deg -> 2.29 rad) (constant)
+        "PD0": 0.035, # keep dropping drag until destroyer can go 520ft at 70 mph
+        #"CD0": 0.042,  # drag at 0 AoA  (based on disc speed)  2 deg AoA at 58mph was about 21m/s^2 with wobble, but only 5 after wobble subsides
+        # (.055 at speed 11, .061 speed 5, .067 speed 4, .083 speed 2)
+        "PDa": 1.67,  # quadratic with AoA from zero lift point (constant)
+        "PTxwz": 0,  # rolling moment related to spin precession?
+        "PTy0": -0.02, # pitching moment from disc stability at 0 AoA (based on turn of disc, also based on cavity of disc)
+        # -0.02 turn -1, -0.007 turn 1, -0.033 turn -2, -0.015 turn 0  (per degree not per rad)
+        "PTya": 0.3, # pitching moment from disc stability linear in AoA (0.006 / deg -> 0.343 / rad) (based on fade of disc)
+        # fade 0 0.002, fade 1 0.004, fade 3  0.006, fade 5 0.008  (per degree not per rad)
+        "PTxwx": -6.0e-4,  # dampening factor for wobble (constant) 21.5 -> 3.3 over 1s
+        "PTzwz": -2.1e-5,  # spin down (constant) at 58mph spindown is about 24m/s^2 (3.5% (118.5 -> 114.5) over .82s)
+        "I_xx": 6.263E-04 * 165/175, # I_xx is much closer to 1/2 on the destroyer than the condor, height is 2.2 vs 1.4
+        # frequency of wobble for destroyer is .98 the rate of rotation.
+        # this means that I_xx is about 1.01 * I_zz / 2
+        # this means if we analyze the
+        "I_zz": 1.231E-03 * 165/175,
+        "mass": 0.165,
+        "diameter": 0.211,
+        "rim_depth": 0.012,
+        "rim_width": 0.0229,
         "height": 0.014,
     })
 
