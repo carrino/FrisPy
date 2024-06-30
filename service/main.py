66009c719eb9d5c4cb695f20b1b83b46cf607fa1
John Carrino <john.carrino@gmail.com>
2024-06-29 22:39:20 -0700

    change port to 8000

--- service/main.py
+++ service/main.py
@@ -141,4 +141,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
